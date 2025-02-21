de82b185a05f17bd37e43673bbac9b9f44c56e20
Lokesh <thadikamallalokesh7@gmail.com>
2025-02-22 00:39:05 +0530

    removed firebase cofigarations

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -7,15 +7,6 @@ import { getFirestore } from "firebase/firestore";
  * so they are hardcoded here temporarily.
  * For security reasons, im removing these hardcoded keys.
  */
-const firebaseConfig = {
-  apiKey: "AIzaSyCVYMeJNg_svOX-HZmTDEfSQ25i3VdxLjs",
-  authDomain: "task-manager-bb1d4.firebaseapp.com",
-  projectId: "task-manager-bb1d4",
-  storageBucket: "task-manager-bb1d4.firebasestorage.app",
-  messagingSenderId: "701552495502",
-  appId: "1:701552495502:web:e2dbbb97aaf15fac76b2df",
-  measurementId: "G-JNG806NLHN"
-};
 
 // const firebaseConfig = {
 //   apiKey: process.env.REACT_APP_FIREBASE_API_KEY ,
